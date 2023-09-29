2c91adff0c0ef09666e84bdfc0156917b6f06f88
Alex Grant <alex@localnerve.com>
2023-09-28 21:49:57 -0400

    @1.0.1 - devdep updates, posttranspile update

--- posttranspile.js
+++ posttranspile.js
@@ -6,8 +6,8 @@ const files = await fs.readdir(dir);
 for (const file of files) {
   const filename = `${dir}${path.sep}${file}`;
   const data = await fs.readFile(filename, 'utf8');
-  const newData = data.replace(/(require\("[a-z./]+)(\.js"\);)/g, (m, req) => {
-    return `${req}.cjs");`;
+  const newData = data.replace(/(require\("[a-z./]+)(\.js")(.+)$/mg, (m, req, repl, rest) => {
+    return `${req}.cjs"${rest}`;
   });
   await fs.writeFile(filename, newData);
 }
